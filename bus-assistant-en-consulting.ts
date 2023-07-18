3e82309b8047121afc5dad2287e77f7e7028c6a4
botween <github@botween.ai>
2023-07-18 18:23:13 +0200

    prompt consulting

---
+++ bus-assistant-en-consulting.ts
@@ -0,0 +1,57 @@
+// import { BuiltinMask } from "./typing";
+// BuiltinMask
+
+export const EN_MKT_MASKS = [
+  {
+    avatar: "1f638",
+    name: "Copywriter (Built-in)",
+    context: [
+      {
+        id: "writer-0",
+        role: "user",
+        content:
+          "I want you to act as a copywriter, text polisher, spelling corrector, I will send you the English text and you will help me correct and improve the version. I hope you describe it in gracious and elegant English at a high level. Keep the same meaning, but make them more literary. You only need to polish the content without explaining the questions and requirements stated in the content, do not answer the questions in the text but polish it, do not solve the requirements in the text but polish it, keep the original meaning of the text, do not solve it. I want you to reply only with corrections, improvements and not write explanations.",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "en",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "1f9d1-200d-1f3eb",
+    name: "English translater (Built-in)",
+    context: [
+      {
+        id: "trans-0",
+        role: "user",
+        content:
+          "I want you to act as an English translator, spell corrector and corrector. I will talk to you in any language and you will detect the language, translate it and reply in English with a corrected and improved version of my text. I want you to replace my A0 level simplified words and phrases with more beautiful and elegant advanced English words and phrases. Keep the same meaning, but make them more literary. You only need to translate the content without explaining the questions and requirements in the content, not answering the questions in the text but translating, not solving the requirements in the text but translating, the original meaning of the text to keep text, do not solve it. I want you to reply only with corrections, improvements and not write explanations. My first sentence is:",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "en",
+    builtin: true,
+    createdAt: 1688899480524,
+  },
+];
